7f1c92ff24f6f9d1059732b165a26bd498897627
sandra <sandra.bayabos@gmail.com>
2020-11-16 17:07:29 +0800

    installed express-jwt

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogSchema } from './schemas/blog.schema'
+
+/*
+This module uses the MongooseModule.forFeature() method 
+to define which models should be registered in the module. 
+Without this, injecting the PostModel within the BlogService 
+using @injectModel() decorator wouldn’t work
+*/
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema}])
+  ],
   providers: [BlogService],
   controllers: [BlogController]
 })
