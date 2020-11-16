9785c2d28c276b74c5741b0f5f7dcb52dfb98e39
sandra <sandra.bayabos@gmail.com>
2020-11-16 18:30:49 +0800

    edited algorithm to algorithms

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,9 +1,9 @@
-import { Module, MiddlewareConsumer, NestModule, RequestMethod } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogSchema } from './schemas/blog.schema';
-import { AuthenticationMiddleware } from 'src/common/authentication.middleware'
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 /*
 This module uses the MongooseModule.forFeature() method 
@@ -14,10 +14,10 @@ using @injectModel() decorator wouldn’t work
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema}])
-  ],
-  providers: [BlogService],
-  controllers: [BlogController]
+      MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema }]),
+  ], // add this
+providers: [BlogService],
+controllers: [BlogController]
 })
 
 /*
@@ -26,12 +26,12 @@ any subsequent requests without an Access Token to the following routes
 will not be allowed by the application
 
 */
-export class BlogModule implements NestModule{
+export class BlogModule implements NestModule {
   configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
     consumer.apply(AuthenticationMiddleware).forRoutes(
-      {method: RequestMethod.POST, path: '/blog/post'},
-      {method: RequestMethod.PUT, path: '/blog/edit'},
-      {method: RequestMethod.DELETE, path: 'blog/delete'}
+      { method: RequestMethod.POST, path: '/blog/post' },
+      { method: RequestMethod.PUT, path: '/blog/edit' },
+      { method: RequestMethod.DELETE, path: '/blog/delete' }
     )
   }
 }
--- src/common/authentication.middleware.ts
+++ src/common/authentication.middleware.ts
@@ -3,7 +3,7 @@ import * as jwt from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
 /*
 
@@ -17,23 +17,23 @@ indicating that the authorization token is not valid.
 export class AuthenticationMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     jwt({
-      secret: expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://${process.env.AUTH0_DOMAIN}/well-known/jwks.json`
-      }),
-      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-      algorithm: ['RS256'],
-    }) (req, res, (err) => {
-      if (err) {
-        const status = err.status || 500;
-        const message = err.message || 'Sorry, we were unable to process your request';
-        return res.status(status).send({
-          message,
-        });
-      }
-      next();
-    });
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+        }),
+        issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+        algorithms: ['RS256'],
+      })(req, res, (err) => {
+        if (err) {
+          const status = err.status || 500;
+          const message = err.message || 'Sorry we were unable to process your request.';
+          return res.status(status).send({
+            message,
+          });
+        }
+        next();
+      });
   }
 }
